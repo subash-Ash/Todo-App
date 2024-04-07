c0bd16568c6c1c9d574b32f9bf0482f2474b8862
subash-Ash <subashsivakumar34@gmail.com>
2024-04-07 10:52:39 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TODO';
+    taskName = '';
+    tasks : WritableSignal<string[]> = signal([]);
+
+    addTask(task : HTMLInputElement) : void{
+        const item = task.value;
+        this.tasks.update((tasks)=>[item, ...tasks]);
+        task.value = '';
+        task.focus();
+      
+    }
+
+    removeTask(index : number) : void {
+      this.tasks.update((tasks) => tasks.filter((_, i) => i !== index));
+    }
 }
